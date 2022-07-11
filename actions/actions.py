f8b0ee6c18c208b3d13df51970656ad9bf2c953f
Fernando Barbosa <ofernandobarbosa@gmail.com>
2022-07-11 10:25:52 -0300

    updade: data training

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,48 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from rasa_sdk.forms import FormValidationAction
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+
+class ValidaNomeForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_name_form"
+
+    async def validate_name(self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        name = slot_value
+        sai = tracker.get_slot("sender_id")
+        if(sai == None):
+            volta = "!"
+            sai = tracker.sender_id
+        else:
+            volta = ", fico feliz com tua volta!"
+
+        texto = "Olá " + name+volta+" O que vai hoje?"
+        dispatcher.utter_message(text=texto)
+        print(name)
+
+        return {"name": name, "sender_id": sai}
