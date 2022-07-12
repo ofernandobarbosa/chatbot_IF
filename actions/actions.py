33722055ef37abe992e343d59e14496d8a77180b
Fernando Barbosa <ofernandobarbosa@gmail.com>
2022-07-12 17:03:30 -0300

    update: story get professor contact

--- actions/actions.py
+++ actions/actions.py
@@ -13,6 +13,7 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 from rasa_sdk.forms import FormValidationAction
+from rasa_sdk.events import SlotSet
 
 
 class ActionHelloWorld(Action):
@@ -28,36 +29,39 @@ class ActionHelloWorld(Action):
 
         return []
 
+
 class BuscarProfessor(Action):
-    
+
     def name(self) -> Text:
         return "action_buscar_professor"
-    
+
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        nome_professor = dispatcher.get_slot("professor_name")
+
+        nome_professor = tracker.get_slot("professor_name")
 
         dispatcher.utter_message(text="fulano@riogrande.ifrs.edu.br")
-        return[]
+
+        return[SlotSet("professor_name", None)]
 
 
 class BuscarFaq(Action):
-    
+
     def name(self) -> Text:
         return "action_faq_comprovate"
-    
+
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         link_matricula = "https://sia.ifrs.edu.br/aplicacoes/frame/index.php"
         link_tutorial = "https://sia.ifrs.edu.br/aplicacoes/frame/index.php"
-        
 
-        dispatcher.utter_message(text=f"Para baixar o comprovante de matrícula você precisa acessar o link {link_matricula}")
-        dispatcher.utter_message(text=f"Caso precise de alguma ajuda, assista no link {link_tutorial}")
+        dispatcher.utter_message(
+            text=f"Para baixar o comprovante de matrícula você precisa acessar o link {link_matricula}")
+        dispatcher.utter_message(
+            text=f"Caso precise de alguma ajuda, assista no link {link_tutorial}")
 
         return []
 
@@ -67,11 +71,11 @@ class ValidaNomeForm(FormValidationAction):
         return "validate_name_form"
 
     async def validate_name(self,
-        slot_value: Any,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: DomainDict,
-    ) -> Dict[Text, Any]:
+                            slot_value: Any,
+                            dispatcher: CollectingDispatcher,
+                            tracker: Tracker,
+                            domain: DomainDict,
+                            ) -> Dict[Text, Any]:
         name = slot_value
         sai = tracker.get_slot("sender_id")
         if(sai == None):
@@ -85,4 +89,3 @@ class ValidaNomeForm(FormValidationAction):
         print(name)
 
         return {"name": name, "sender_id": sai}
-
