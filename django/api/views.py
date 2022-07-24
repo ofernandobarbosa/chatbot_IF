92cc96a0248039b2a07c0d13e55a526246c8a6a3
Anderson de Aguiar de Oliveira <andersondeaguiardeoliveira@gmail.com>
2022-07-24 19:59:19 -0300

    Adicionei versão final do Sistema de Controle de Informações do IFRS
    Serviço Web - OK
    Banco de Dados - OK
    Api Rest - OK

--- django/api/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from app.models import Calendario
-from api.serializer import CalendarioSerializer
-
-class CalendariosViewSet(viewsets.ModelViewSet):
-    """Todos Eventos"""
-    queryset = Calendario.objects.all()
-    serializer_class = CalendarioSerializer
