b5f825e355df9e4ef3d40413f334c02fec28bba4
Anderson de Aguiar de Oliveira <andersondeaguiardeoliveira@gmail.com>
2022-08-01 00:28:42 -0300

    editei aplicação api para limitar utilização dela e adaptei front da aplicação principal nela

--- sistema-de-controle-de-informacoes-ifrs/api/views.py
+++ sistema-de-controle-de-informacoes-ifrs/api/views.py
@@ -1,15 +1,23 @@
-from rest_framework import viewsets
+from rest_framework import generics
+from django.shortcuts import render
 from app.models import Evento
 from categorias.models import Categorias
-from api.serializer import EventoSerializer, comprovante_de_matriculaSerializer, contato_dos_professoresSerializer, grade_de_horariosSerializer, calendario_academicoSerializer, informacoes_relevantes_dos_cursosSerializer, informacoes_sobre_inscricao_ou_matriculaSerializer, informacoes_sobre_rematriculaSerializer, requerimentos_ou_formulariosSerializer, tutoriais_de_acessos_a_sistemas_academicosSerializer
+from api.serializer import Eventos_todosSerializer, comprovante_de_matriculaSerializer, contato_dos_professoresSerializer, grade_de_horariosSerializer, calendario_academicoSerializer, informacoes_relevantes_dos_cursosSerializer, informacoes_sobre_inscricao_ou_matriculaSerializer, informacoes_sobre_rematriculaSerializer, requerimentos_ou_formulariosSerializer, tutoriais_de_acessos_a_sistemas_academicosSerializer
 from app.views import categoria
 
-class EventosViewSet(viewsets.ModelViewSet):
-    """Todos Eventos"""
+
+def api(request):
+    categorias = Categorias.objects.all()
+
+    dados = {
+        'categorias': categorias,
+    }
+    return render(request, 'api.html', dados)
+class Eventos_todos(generics.ListAPIView):
     queryset = Evento.objects.all()
-    serializer_class = EventoSerializer
+    serializer_class = Eventos_todosSerializer
     
-class comprovante_de_matriculaViewSet(viewsets.ModelViewSet):
+class comprovante_de_matricula(generics.ListAPIView):
     """comprovante_de_matricula"""
     cat = Categorias.objects.all()
     global id
@@ -19,7 +27,7 @@ class comprovante_de_matriculaViewSet(viewsets.ModelViewSet):
     queryset = Evento.objects.all().order_by('-data_atualizacao').filter(categoria=id).filter(visivel=True)
     serializer_class = comprovante_de_matriculaSerializer
 
-class contato_dos_professoresViewSet(viewsets.ModelViewSet):
+class contato_dos_professores(generics.ListAPIView):
     """contato_dos_professores"""
     cat = Categorias.objects.all()
     global id
@@ -29,7 +37,7 @@ class contato_dos_professoresViewSet(viewsets.ModelViewSet):
     queryset = Evento.objects.all().order_by('-data_atualizacao').filter(categoria=id).filter(visivel=True)
     serializer_class = contato_dos_professoresSerializer
     
-class grade_de_horariosViewSet(viewsets.ModelViewSet):
+class grade_de_horarios(generics.ListAPIView):
     """grade_de_horarios"""
     cat = Categorias.objects.all()
     global id
@@ -39,7 +47,7 @@ class grade_de_horariosViewSet(viewsets.ModelViewSet):
     queryset = Evento.objects.all().order_by('-data_atualizacao').filter(categoria=id).filter(visivel=True)
     serializer_class = grade_de_horariosSerializer
     
-class calendario_academicoViewSet(viewsets.ModelViewSet):
+class calendario_academico(generics.ListAPIView):
     """calendario_academico"""
     cat = Categorias.objects.all()
     global id
@@ -49,7 +57,7 @@ class calendario_academicoViewSet(viewsets.ModelViewSet):
     queryset = Evento.objects.all().order_by('-data_atualizacao').filter(categoria=id).filter(visivel=True)
     serializer_class = calendario_academicoSerializer
     
-class informacoes_relevantes_dos_cursosViewSet(viewsets.ModelViewSet):
+class informacoes_relevantes_dos_cursos(generics.ListAPIView):
     """informacoes_relevantes_dos_cursos"""
     cat = Categorias.objects.all()
     global id
@@ -59,7 +67,7 @@ class informacoes_relevantes_dos_cursosViewSet(viewsets.ModelViewSet):
     queryset = Evento.objects.all().order_by('-data_atualizacao').filter(categoria=id).filter(visivel=True)
     serializer_class = informacoes_relevantes_dos_cursosSerializer
     
-class informacoes_sobre_inscricao_ou_matriculaViewSet(viewsets.ModelViewSet):
+class informacoes_sobre_inscricao_ou_matricula(generics.ListAPIView):
     """informacoes_sobre_inscricao_ou_matricula"""
     cat = Categorias.objects.all()
     global id
@@ -69,7 +77,7 @@ class informacoes_sobre_inscricao_ou_matriculaViewSet(viewsets.ModelViewSet):
     queryset = Evento.objects.all().order_by('-data_atualizacao').filter(categoria=id).filter(visivel=True)
     serializer_class = informacoes_sobre_inscricao_ou_matriculaSerializer
     
-class informacoes_sobre_rematriculaViewSet(viewsets.ModelViewSet):
+class informacoes_sobre_rematricula(generics.ListAPIView):
     """informacoes_sobre_rematricula"""
     cat = Categorias.objects.all()
     global id
@@ -79,7 +87,7 @@ class informacoes_sobre_rematriculaViewSet(viewsets.ModelViewSet):
     queryset = Evento.objects.all().order_by('-data_atualizacao').filter(categoria=id).filter(visivel=True)
     serializer_class = informacoes_sobre_rematriculaSerializer
     
-class requerimentos_ou_formulariosViewSet(viewsets.ModelViewSet):
+class requerimentos_ou_formularios(generics.ListAPIView):
     """requerimentos_ou_formularios"""
     cat = Categorias.objects.all()
     global id
@@ -89,7 +97,7 @@ class requerimentos_ou_formulariosViewSet(viewsets.ModelViewSet):
     queryset = Evento.objects.all().order_by('-data_atualizacao').filter(categoria=id).filter(visivel=True)
     serializer_class = requerimentos_ou_formulariosSerializer
     
-class tutoriais_de_acessos_a_sistemas_academicosViewSet(viewsets.ModelViewSet):
+class tutoriais_de_acessos_a_sistemas_academicos(generics.ListAPIView):
     """tutoriais_de_acessos_a_sistemas_academicos"""
     cat = Categorias.objects.all()
     global id
